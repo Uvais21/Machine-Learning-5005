2d3b4cade81bb23fe82596a3f31940c0044c4868
Uvais21 <116837143+Uvais21@users.noreply.github.com>
2024-02-13 01:20:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,3 @@ install_requires = [] # Here we'll add: ["gunicorn", "docutils>=0.3", "lxml==0.5
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
-
-setup(name='aim5005',
-      version='0.1',
-      description='Project for Katz AIM-5005',
-      url='http://github.com/mzelenetz/aim5005',
-      author='Michael Zelenetz',
-      author_email='michael.zelenetz@yu.edu',
-      license='MIT',
-      packages=['aim5005'],
-      install_requires=install_requires,
-      zip_safe=False)
